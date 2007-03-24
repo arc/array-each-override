2c910d62ccffbcce87a8f3bb697a1a6dda153bc4
Aaron Crane <arc@cpan.org>
2007-03-24 18:23:06 +0000

    Benchmark everything in most contexts

--- benchmark.pl
+++ benchmark.pl
@@ -7,7 +7,7 @@ use blib;
 
 use Benchmark qw<cmpthese>;
 use File::Find qw<find>;
-use Array::Each::Override qw<array_each>;
+use Array::Each::Override qw<:safe>;
 
 my %hash;
 find({no_chdir => 1, wanted => sub {
@@ -19,7 +19,32 @@ find({no_chdir => 1, wanted => sub {
     }
 } }, '.');
 
+print "\neach %hash:\n";
 cmpthese(-10, {
     core => sub { my $n = 0;  while (my ($k, $v) = CORE::each %hash) { $n++ } },
     mine => sub { my $n = 0;  while (my ($k, $v) = array_each %hash) { $n++ } },
+});
+
+print "\nscalar keys %hash:\n";
+cmpthese(-10, {
+    core => sub { my $n = CORE::keys %hash },
+    mine => sub { my $n = array_keys %hash },
+});
+
+print "\nscalar values %hash:\n";
+cmpthese(-10, {
+    core => sub { my $n = CORE::values %hash },
+    mine => sub { my $n = array_values %hash },
+});
+
+print "\nlist keys %hash:\n";
+cmpthese(-10, {
+    core => sub { my $n = 0; $n++ for CORE::keys %hash },
+    mine => sub { my $n = 0; $n++ for array_keys %hash },
+});
+
+print "\nlist values %hash:\n";
+cmpthese(-10, {
+    core => sub { my $n = 0; $n++ for CORE::values %hash },
+    mine => sub { my $n = 0; $n++ for array_values %hash },
 });
